757c6966eb7f08f02dd516e6c4f515253a09f5a9
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-04-21 12:25:24 +0300

    pdf fix

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,6 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/usr/bin/wkhtmltopdf'
-}
+if Rails.env.staging? || Rails.env.production?
+  exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+else
+  exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
+  # exe_path = '/usr/local/bin/wkhtmltopdf'
+end
