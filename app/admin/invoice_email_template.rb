d7e06e6033cfa68c66e7a6f4fabe47392bd66bf0
Sergey Poustovit <levellevel4@gmail.com>
2015-05-18 19:22:24 +0300

    Add button Save for invoice email template & html_safe added at index page

--- app/admin/invoice_email_template.rb
+++ app/admin/invoice_email_template.rb
@@ -2,6 +2,19 @@ ActiveAdmin.register InvoiceEmailTemplate do
   permit_params :template_subject, :template_body, :email_templatable_id,\
                 :email_templatable_type, :to, :cc, :from, :name
 
+  index do
+    selectable_column
+    column :template_subject
+    column(:template_body) { |obj| obj.template_body.html_safe }
+    column :email_templatable_id
+    column :email_templatable_type
+    column :to
+    column :cc
+    column :from
+    column :name
+    column :created_at
+  end
+
   form do |f|
     f.inputs "InvoiceEmailTemplate" do
       f.input :name
@@ -11,5 +24,6 @@ ActiveAdmin.register InvoiceEmailTemplate do
       f.input :from
       f.input :template_body
     end
+    actions
   end
 end
