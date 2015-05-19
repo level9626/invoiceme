1853af03ce5e0d114f31c4f4c7e98c8a9a2f8d69
Sergey Poustovit <levellevel4@gmail.com>
2015-05-19 14:58:49 +0300

    Added ability to filter invoices by clicking currencies in statistic info

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -15,6 +15,12 @@ class InvoicesController < ApplicationController
     @search = _search
     @invoices = @search.result.paginate(per_page: 10, page: params[:page])
 
+    if params[:currency]
+      @invoices = Invoice.where(currency: params[:currency]).paginate(per_page: 10, page: params[:page])
+    elsif params[:currency] && params[:state]
+      @invoices = Invoice.where(currency: params[:currency], state: params[:state]).paginate(per_page: 10, page: params[:page])
+    end
+
     respond_to do |format|
       format.html
       format.csv { send_data @invoices.to_csv }
