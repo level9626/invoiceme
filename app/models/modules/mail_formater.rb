8a1c1c62851d61605a7711f2e2bd7c814ee6bea0
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-05-26 10:01:32 +0300

    views fix

--- app/models/modules/mail_formater.rb
+++ app/models/modules/mail_formater.rb
@@ -4,6 +4,10 @@ module Modules
       define_method method.to_s + '=' do |emails|
         self[method] = emails.to_a.map(&:strip)
       end
+
+      define_method method.to_s do
+        self[method].join(', ')
+      end
     end
   end
 end
