c722063a880d55943d3071f65bc797f77dd292e0
Sergey Poustovit <levellevel4@gmail.com>
2015-06-12 11:26:24 +0300

    Attachments removed from payments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   has_many :attachments, as: :attachable
 
+  accepts_nested_attributes_for :attachments, allow_destroy: true
+
   validates :content, presence: true, length: { maximum: 5 }
 end
