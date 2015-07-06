0eee291b7a9dc48d693aefff2d31414cf7a5fba7
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-07-07 01:40:38 +0300

    filter finished

--- app/assets/javascripts/directives/in_filter.js
+++ app/assets/javascripts/directives/in_filter.js
@@ -12,27 +12,46 @@ angular.module('InvMe')
                   <div class="panel-heading"> \
                     <panel-title class="filter_text">Filters</panel-title> \
                     <span class="pull-right clickable"> \
-                      <i class="glyphicon glyphicon-chevron-down filter_glyph"></i> \
+                      <i class="glyphicon glyphicon-chevron-down filter_glyph" ng-click="toggleShow()"></i> \
                     </span> \
                   </div> \
                   <div class="panel-body dropdown_panel"> \
                     <form ng-submit="filter()"> \
                       <ng-transclude></ng-transclude> \
                       <button class="btn btn-round btn-primary" type="submit">Filter</button> \
+                      <div class="pull-right"> \
+                        <button class="btn btn-round btn-danger" ng-click="clearFilter()">Clear Filters</button> \
                     </form> \
                   </div> \
                 </div>',
       link: function(scope, element){
         // Initialize and pre-populate filter form
-        var form = element.find('form').serializeObject()
+        var form = element.find('form'),
+            formData = form.serializeObject(),
+            hiddable = element.find('.panel-body');
 
-        for (var k in form) {
+        hiddable.hide();
+
+        for (var k in formData) {
           element.find("[name='"+k+"']").val($location.search()[k]);
         }
+        // END init;
 
         scope.filter = function () {
-          $location.search($.param(element.find('form').serializeObject()));
+          $location.search($.param(form.serializeObject()));
         };
+
+        scope.clearFilter = function () {
+          $location.search('');
+        }
+
+        scope.toggleShow = function () {
+          if (hiddable.is(":visible")) {
+            hiddable.hide();
+          } else {
+            hiddable.show();
+          }
+        }
       }
     };
   }]);
