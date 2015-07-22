e22b86da83fca8240236a4ebdbcbae32c0640074
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-07-23 00:42:42 +0300

    show/hide editor

--- app/assets/javascripts/directives/rich-text-editor.js
+++ app/assets/javascripts/directives/rich-text-editor.js
@@ -7,7 +7,7 @@ angular.module('InvMe')
       transclude: true,
       template: '<div><textarea></textarea></div>',
       link: function(scope, element, attrs, controller) {
-        var editor, textarea;
+        var editor, textarea, toolbar, button;
         textarea = element.find('textarea').wysihtml5({
           toolbar: {
               "font-styles": false,
@@ -20,6 +20,33 @@ angular.module('InvMe')
           }
         });
         editor = textarea.data('wysihtml5').editor;
+        toolbar = textarea.data('wysihtml5').toolbar;
+
+        // show/hide editor toolbar
+        button = $('\
+          <button id="show-toolbar" \
+                  class="btn-primary pull-right btn-xs" \
+                  type="button" \
+                  style=" margin-bottom: 8px;"> \
+            Show Editor \
+          </button>');
+
+        toolbar.parent().prepend(button);
+
+        toolbar.addClass('hidden');
+
+        button.on('click', function(){
+          if (toolbar.hasClass('hidden')){
+            button.text("Hide editor");
+            toolbar.removeClass('hidden');
+          }else{
+            button.text("Show editor");
+            toolbar.addClass('hidden')   ;
+          }
+          return false;
+        });
+
+        // update editor content on model change
         editor.on('change', function() {
           return controller.$setViewValue(editor.getValue());
         });
