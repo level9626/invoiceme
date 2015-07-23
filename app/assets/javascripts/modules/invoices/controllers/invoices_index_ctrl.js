a874697d9032b02616446cf742e59c423fedec47
Sergey Poustovit <levellevel4@gmail.com>
2015-07-23 12:59:45 +0300

    Table sorting added

--- app/assets/javascripts/modules/invoices/controllers/invoices_index_ctrl.js
+++ app/assets/javascripts/modules/invoices/controllers/invoices_index_ctrl.js
@@ -7,8 +7,14 @@ angular.module('InvoicesApp')
     'Invoice',
     function ($scope, $location, Invoice) {
 
-      Invoice.query($location.search(), function (data) {
-        $scope.invoices = data.invoices;
-      });
+        Invoice.query($location.search(), function (data) {
+            $scope.invoices = data.invoices;
+        });
+
+
+        $scope.sortType     = 'invoice_number'; // set the default sort type
+        $scope.sortReverse  = false;  // set the default sort order
+
+
 
     }]);
