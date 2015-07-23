4889c5ede1a15d075698fcac69a461b382318ca0
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-07-23 19:07:25 +0300

    sort button

---
+++ app/assets/javascripts/directives/sort_button.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('InvMe')
+  .directive('sortButton',
+    ['$location',
+    function($location){
+    return {
+      restrict: 'A',
+      transclude: true,
+      template: '<span ng-click="sort()"><ng-transclude></ng-transclude></span>',
+      scope: {
+        sortButton: '@'
+      },
+      link: function(scope, element){
+        // Initialize and pre-populate filter form
+        scope.palarity = true;
+
+        scope.sort = function () {
+          $location.search($.param({q: {s: scope.sortButton + _getPol() } }));
+          scope.palarity = !scope.palarity;
+        };
+
+        function _getPol () {
+          if (scope.palarity) return ' asc';
+          return ' desc'
+        }
+      }
+    };
+  }]);
