f6221bb4c5fb897adf4f0a6a63b641185de18912
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-08-04 11:27:29 +0300

    company form in material dialog

--- app/assets/javascripts/modules/companies/controllers/companies_index_ctrl.js
+++ app/assets/javascripts/modules/companies/controllers/companies_index_ctrl.js
@@ -1,17 +1,35 @@
 'use strict';
 
+function DialogController($scope, $mdDialog) {
+  $scope.submit = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+}
+
 angular.module('CompaniesApp')
   .controller('CompaniesIndexCtrl',
   ['$scope',
+   '$mdDialog',
    'Company',
-   function ($scope, Company) {
+   function ($scope, $mdDialog, Company) {
 
     _init();
 
     $scope.destoryCompany = function (company_id) {
       Company.remove({id: company_id});
       _init();
-    }
+    };
+
+    $scope.newCompany = function (ev) {
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: 'companies/new.html',
+        targetEvent: ev,
+      });
+    };
 
     function _init() {
       Company.query(function (data) {
