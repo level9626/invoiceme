845cb8f3a58090e43096adbeb76d673eceab7a25
Sergey Poustovit <levellevel4@gmail.com>
2015-08-17 10:19:09 +0300

    Reformat few directives

--- app/assets/javascripts/directives/sort_button.js
+++ app/assets/javascripts/directives/sort_button.js
@@ -2,28 +2,28 @@
 
 angular.module('InvMe')
   .directive('sortButton',
-    ['$location',
-    function($location){
-    return {
-      restrict: 'A',
-      transclude: true,
-      template: '<span ng-click="sort()"><ng-transclude></ng-transclude></span>',
-      scope: {
-        sortButton: '@'
-      },
-      link: function(scope, element){
-        // Initialize and pre-populate filter form
-        scope.palarity = true;
+  ['$location',
+    function ($location) {
+      return {
+        restrict: 'A',
+        transclude: true,
+        template: '<span ng-click="sort()"><ng-transclude></ng-transclude></span>',
+        scope: {
+          sortButton: '@'
+        },
+        link: function (scope, element) {
+          // Initialize and pre-populate filter form
+          scope.palarity = true;
 
-        scope.sort = function () {
-          $location.search($.param({q: {s: scope.sortButton + _getPol() } }));
-          scope.palarity = !scope.palarity;
-        };
+          scope.sort = function () {
+            $location.search($.param({q: {s: scope.sortButton + _getPol()}}));
+            scope.palarity = !scope.palarity;
+          };
 
-        function _getPol () {
-          if (scope.palarity) return ' asc';
-          return ' desc'
+          function _getPol() {
+            if (scope.palarity) return ' asc';
+            return ' desc'
+          }
         }
-      }
-    };
-  }]);
+      };
+    }]);
