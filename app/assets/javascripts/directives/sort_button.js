6c2460cf5fa5cf12d3d570c159bbb94054f25c9f
Sergey Poustovit <levellevel4@gmail.com>
2015-08-28 16:58:31 +0300

    Sort button polarity change on Invoice index page

--- app/assets/javascripts/directives/sort_button.js
+++ app/assets/javascripts/directives/sort_button.js
@@ -3,12 +3,13 @@
 angular.module('InvMe')
   .directive('sortButton',[
   'Search',
-  function (Search) {
+  '$location',
+  function (Search, $location) {
     return {
       restrict: 'A',
       transclude: true,
       template: '<span class="sort-button" \
-                       ng-class="{active: active()}" \
+                       ng-class="{active: active(), up: up(), down: down()}" \
                        ng-click="sort()"> \
                    <ng-transclude></ng-transclude> \
                  </span>',
@@ -27,6 +28,23 @@ angular.module('InvMe')
         scope.active = function () {
           return Search.q_params()['q[s]'] && Search.q_params()['q[s]'].match(scope.sortButton);
         };
+
+        scope.up = function () {
+          if (_polarity() == ' desc')
+            $('span.active').children().find('i').removeClass('fa fa-sort').addClass('fa fa-caret-up');
+        };
+
+        scope.down = function () {
+          if (_polarity() == ' asc')
+            $('span.active').children().find('i').removeClass('fa fa-sort').addClass('fa fa-caret-down');
+        };
+
+        function _polarity () {
+          if ($location.url().match(/asc/))
+            return ' desc';
+          else if ($location.url().match(/desc/))
+            return ' asc'
+        }
 
         function _getPol() {
           if (scope.palarity) return ' asc';
